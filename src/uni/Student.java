7e2b7d3daf1fbd65afc6aae89471cb9da50921e1
DESKTOP-H33QLTS\cindy <cindy.schwendele@web.de>
2018-04-14 00:50:39 +0200

    Attribut Name hinzugefügt

--- src/uni/Student.java
+++ src/uni/Student.java
@@ -2,4 +2,14 @@ package uni;
 
 public class Student {
 
+		private String Name;
+
+		public String getName() {
+			return Name;
+		}
+
+		public void setName(String name) {
+			Name = name;
+		}
+		
 }
